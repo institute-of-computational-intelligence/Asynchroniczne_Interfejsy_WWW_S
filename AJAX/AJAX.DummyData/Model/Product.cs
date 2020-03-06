dab6939e1f9578cb13b306c4eaeb54d1ac56b243
Justyna R <justyna.rak>
2020-03-06 15:51:13 +0100

    Lab1

---
+++ AJAX/AJAX.DummyData/Model/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AJAX.DummyData.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public Decimal Price { get; set; }
+        public int Amount { get; set; }
+        public DateTime ExpirationDate { get; set; }
+    }
+}
---
+++ AJAX/AJAX.DummyData/Services/ProductService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AJAX.DummyData.Model;
+using Bogus;
+
+namespace AJAX.DummyData.Services
+{
+    class ProductService : IProductService
+    {
+        private IList<Product> _products;
+        private readonly Faker<Product> _productFaker;
+
+
+        public ProductService ()
+        {
+
+            int productIdCount = 1;
+            _products = new List<Product>();
+
+            var availableProducts = new[] { "apple", "banana", "orange" };
+            Randomizer.Seed = new Random(8675309);
+            _productFaker = new Faker<Product>()
+                .StrictMode(true)
+                .RuleFor(x => x.Id, f => productIdCount++)
+                .RuleFor(x => x.Price, f => f.Random.Number(100) + Math.Round(f.Random.Decimal()))
+                .RuleFor(x => x.ExpirationDate, f => f.Date.Recent(0))
+                .RuleFor(x => x.ProductName, f => f.PickRandom(availableProducts))
+                .RuleFor(x => x.Amount, f => f.Random.Number(100) + Math.Round(f.Random.Double(), 2));
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Product> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Post(Product model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Put(Product model, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
