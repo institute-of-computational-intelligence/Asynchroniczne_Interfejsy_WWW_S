0f43fc91f9c48c2d648cc4104a8c35f73e44e9be
Kamil Ch <kamil_chudy@o2.pl>
2020-03-06 15:51:07 +0100

    Lab1

---
+++ AJAX/AJAX.DummyData/Services/ProductService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AJAX.DummyData.Model;
+using Bogus;
+
+namespace AJAX.DummyData.Services
+{
+    public class ProductService : IProductService
+    {
+        private IList<Product> _product;
+        private readonly Faker<Product> _productFake;
+
+        public ProductService()
+        {
+            int productIdCount = 1;
+            _product = new List<Product>();
+            var availableProducts = new[] { "apple", "banana", "strawberry" };
+            Randomizer.Seed = new Random(3456213);
+            _productFake = new Faker<Product>()
+                .StrictMode(true)
+                .RuleFor(x => x.Id, f => productIdCount++)
+                .RuleFor(x => x.Price, f => f.Random.Number(100) + Math.Round(f.Random.Decimal(), 2))
+                .RuleFor(x => x.ExpirationDate, f => f.Date.Recent())
+                .RuleFor(x => x.ProductName, f => f.PickRandom(availableProducts))
+                .RuleFor(x => x.Amount, f => f.Random.Number(100) + Math.Round(f.Random.Double(), 2));
+
+        }
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Product> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Post(Product model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Put(Product model, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
