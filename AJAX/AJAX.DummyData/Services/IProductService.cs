38daf763bfb64ade4c6f6cc9c3fbc6c6b39ad291
Michał Kowalczyk <kowalMic@gmail.com>
2020-03-06 15:50:53 +0100

    lab1

---
+++ AJAX/AJAX.DummyData/Services/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using AJAX.DummyData.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AJAX.DummyData.Services
+{
+    public interface IProductService
+    {
+        IList<Product> Get();
+        Product Get(int id);
+        Product Post(Product model);
+        Product Put(Product model, int id);
+        void Delete(int id);
+
+    }
+}
---
+++ AJAX/AJAX.DummyData/Services/ProductService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AJAX.DummyData.Model;
+using Bogus;
+
+namespace AJAX.DummyData.Services
+{
+    public class ProductService : IProductService
+    {
+        private IList<Product> _products;
+        private readonly Faker<Product> _productFake;
+
+        public ProductService()
+        {
+            int productIdCount = 1;
+            _products = new List<Product>();
+            var availableProducts = new[] { "apple", "bannana", "orange", "strwaberry", "kiwi", "bread" };
+
+            Randomizer.Seed = new Random(8675309);
+            _productFake = new Faker<Product>()
+                .StrictMode(true)
+                .RuleFor(x => x.Id, f => productIdCount++)
+                .RuleFor(x => x.Price, f => f.Random.Number(100) + Math.Round(f.Random.Decimal(),2))
+                .RuleFor(x => x.ExpirationDate, f => f.Date.Recent())
+                .RuleFor(x => x.Name, f => f.PickRandom(availableProducts))
+                .RuleFor(x => x.Amount, f => f.Random.Number(100) + Math.Round(f.Random.Double(),2));
+        }
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Product> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Post(Product model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Put(Product model, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
